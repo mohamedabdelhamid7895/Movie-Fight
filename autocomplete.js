45626ab5d4d610de7d2f400f5c6ca9dfa09eb133
mohamedabdelhamid7895 <54843140+mohamedabdelhamid7895@users.noreply.github.com>
2020-07-08 21:44:41 +0200

    Update autocomplete.js

--- autocomplete.js
+++ autocomplete.js
@@ -1,55 +1,53 @@
-const autoCompleteConfig = {
-    renderOption(movie) {
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-        return `
-      <img src="${imgSrc}" />
-      ${movie.Title} (${movie.Year})
-    `;
-    },
-    inputValue(movie) {
-        return movie.Title;
-    },
-    async fetchData(searchTerm) {
-        const response = await axios.get('http://www.omdbapi.com/', {
-            params: {
-                apikey: '7c80c6f9',
-                s: searchTerm
-            }
-        });
+const createAutoComplete = ({
+    root,
+    renderOption,
+    onOptionSelect,
+    inputValue,
+    fetchData
+}) => {
+    root.innerHTML = `
+    <label><b>Search</b></label>
+    <input class="input" />
+    <div class="dropdown">
+      <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+      </div>
+    </div>
+  `;
 
-        if (response.data.Error) {
-            return [];
+    const input = root.querySelector('input');
+    const dropdown = root.querySelector('.dropdown');
+    const resultsWrapper = root.querySelector('.results');
+
+    const onInput = async event => {
+        const items = await fetchData(event.target.value);
+
+        if (!items.length) {
+            dropdown.classList.remove('is-active');
+            return;
         }
 
-        return response.data.Search;
-    }
-};
+        resultsWrapper.innerHTML = '';
+        dropdown.classList.add('is-active');
+        for (let item of items) {
+            const option = document.createElement('a');
 
-createAutoComplete({
-    ...autoCompleteConfig,
-    root: document.querySelector('#left-autocomplete'),
-    onOptionSelect(movie) {
-        document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
-    }
-});
-createAutoComplete({
-    ...autoCompleteConfig,
-    root: document.querySelector('#right-autocomplete'),
-    onOptionSelect(movie) {
-        document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#right-summary'), 'right');
-    }
-});
+            option.classList.add('dropdown-item');
+            option.innerHTML = renderOption(item);
+            option.addEventListener('click', () => {
+                dropdown.classList.remove('is-active');
+                input.value = inputValue(item);
+                onOptionSelect(item);
+            });
 
-let leftMovie;
-let rightMovie;
-const onMovieSelect = async (movie, summaryElement, side) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: 'd9835cc5',
-            i: movie.imdbID
+            resultsWrapper.appendChild(option);
+        }
+    };
+    input.addEventListener('input', debounce(onInput, 500));
+
+    document.addEventListener('click', event => {
+        if (!root.contains(event.target)) {
+            dropdown.classList.remove('is-active');
         }
     });
-
-    summaryElement.innerHTML = movi
+};
